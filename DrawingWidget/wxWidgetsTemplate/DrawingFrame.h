4c5ff2c85e1a860c8d2b6331978ceeefdd48fbb3
Taeyler <taeyler.lyons@gmail.com>
2021-05-20 01:47:17 -0700

    Update DrawingFrame.h

--- DrawingWidget/wxWidgetsTemplate/DrawingFrame.h
+++ DrawingWidget/wxWidgetsTemplate/DrawingFrame.h
@@ -1,12 +1,6 @@
 /****************************************************************
 File: DrawingFrame.h
 Description: DrawingFrame header file
-
-Author: Taeyler Lyons
-Class: CSCI 120
-Date: 3.30.16
-
-I hereby certify that this program is entirely my own work.
 *****************************************************************/
 
 #ifndef DRAWINGFRAME_H
