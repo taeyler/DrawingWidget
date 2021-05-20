d33318a38d4afb6766ed8d41bae956f34e4389fe
Taeyler <taeyler.lyons@gmail.com>
2021-05-20 01:47:01 -0700

    Update DrawingFrame.cpp

--- DrawingWidget/wxWidgetsTemplate/DrawingFrame.cpp
+++ DrawingWidget/wxWidgetsTemplate/DrawingFrame.cpp
@@ -1,12 +1,6 @@
 /****************************************************************
 File: DrawingFrame.cpp
 Description: DrawingFrame source file
-
-Author: Taeyler Lyons
-Class: CSCI 120
-Date: 3.30.16
-
-I hereby certify that this program is entirely my own work.
 *****************************************************************/
 
 #include "DrawingFrame.h"
