6ff5738680985f0f719675d65b4719e3e2f51409
Taeyler <taeyler.lyons@gmail.com>
2021-05-20 01:51:07 -0700

    Update Triangle.cpp

--- DrawingWidget/wxWidgetsTemplate/Triangle.cpp
+++ DrawingWidget/wxWidgetsTemplate/Triangle.cpp
@@ -1,12 +1,6 @@
 /****************************************************************
 File: Triangle.cpp
 Description: Triangle source file
-
-Author: Taeyler Lyons
-Class: CSCI 120
-Date: 3.30.16
-
-I hereby certify that this program is entirely my own work.
 *****************************************************************/
 
 #include "wx/wx.h"
